6cf0dc2fc86df61de470917752e6565b84486332
Chongyun Lee <45286352+licy183@users.noreply.github.com>
2023-04-05 02:27:52 +0800

    bump(tur/python-pandas): 2.0.0

--- tur/python-pandas/build.sh
+++ tur/python-pandas/build.sh
@@ -3,13 +3,12 @@ TERMUX_PKG_HOMEPAGE=https://pandas.pydata.org/
 TERMUX_PKG_DESCRIPTION="Powerful Python data analysis toolkit"
 TERMUX_PKG_LICENSE="BSD 3-Clause"
 TERMUX_PKG_MAINTAINER="@termux-user-repository"
-TERMUX_PKG_VERSION=1.5.3
-TERMUX_PKG_SRCURL=https://github.com/pandas-dev/pandas/archive/refs/tags/v${TERMUX_PKG_VERSION}.tar.gz
-TERMUX_PKG_SHA256=d8abf9c2bf33cac75b28f32c174c29778414eb249e5e2ccb69b1079b97a8fc66
+TERMUX_PKG_VERSION=2.0.0
+TERMUX_PKG_SRCURL=git+https://github.com/pandas-dev/pandas.git
 TERMUX_PKG_AUTO_UPDATE=true
 TERMUX_PKG_DEPENDS="libc++, python, python-numpy, python-pip"
-TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, numpy, wheel"
-TERMUX_PKG_PYTHON_TARGET_DEPS="python-dateutil, pytz"
+TERMUX_PKG_PYTHON_COMMON_DEPS="Cython, numpy, setuptools>=61.0.0, versioneer[toml], wheel"
+TERMUX_PKG_PYTHON_TARGET_DEPS="python-dateutil>=2.8.2, pytz>=2020.1, tzdata>=2022.1"
 TERMUX_PKG_BUILD_IN_SRC=true
 
 termux_step_pre_configure() {
